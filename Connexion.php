45695c274e62d42098ee30bcf92733553052dba3
Your Name <you@example.com>
2023-02-22 23:49:23 +0100

    first commit

---
+++ Connexion.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form action="inscription-traitement.php" method="POST">
+        <input type="email" name="" id="" placeholder="email"><br> <br>
+        <input type="password" name="" id="" placeholder="Password"><br><br>
+        <button>Connecter</button>
+    </form>
+</body>
+</html>
